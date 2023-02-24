877206ea58a26001c391e55d3532b6ae7e5ab8d0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-24 09:46:58 +0530

    Swamy/23feb work (#2)
    * Rabbit MQ Settings
    
    * Mass Transit Extension
    
    * Update CommonLibrary.csproj

---
+++ src/CommonLibrary/MassTransit/Extensions.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using CommonLibrary.Settings;
+using MassTransit;
+using MassTransit.Definition;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Play.Common.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services)
+    {
+        services.AddMassTransit(configure =>
+        {
+            configure.AddConsumers(Assembly.GetEntryAssembly());
+
+            configure.UsingRabbitMq((context, configurator) =>
+            {
+                var configuration = context.GetService<IConfiguration>();
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                configurator.Host(rabbitMQSettings?.Host);
+                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings?.ServiceName, false));
+            });
+        });
+
+        services.AddMassTransitHostedService();
+
+        return services;
+    }
+}
