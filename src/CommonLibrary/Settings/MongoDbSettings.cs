7c5c07acc00d49f577c18c89455ae8edb84cbf37
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-04-27 10:25:58 +0530

    Swamy/26apr work (#17)
    * Update MongoDbSettings.cs
    
    * Update CommonLibrary.csproj

--- src/CommonLibrary/Settings/MongoDbSettings.cs
+++ src/CommonLibrary/Settings/MongoDbSettings.cs
@@ -2,9 +2,21 @@
 
 public class MongoDbSettings
 {
+    private string? _connectionString;
+
     public string? Host { get; init; }
 
     public int Port { get; init; }
 
-    public string ConnectionString => $"mongodb://{Host}:{Port}";
+    public string ConnectionString
+    {
+        get
+        {
+            return string.IsNullOrWhiteSpace(_connectionString) ? $"mongodb://{Host}:{Port}" : _connectionString;
+        }
+
+        init { _connectionString = value; }
+    }
 }
+
+// public string ConnectionString => $"mongodb://{Host}:{Port}";
