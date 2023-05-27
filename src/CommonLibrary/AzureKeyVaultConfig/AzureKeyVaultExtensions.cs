eb331a67f2a34ead4db0d1ed4804c6a415706468
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-27 20:01:54 +0530

    ConfigureAzureKeyVault

---
+++ src/CommonLibrary/AzureKeyVaultConfig/AzureKeyVaultExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Identity;
+using CommonLibrary.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace CommonLibrary.AzureKeyVaultConfig;
+
+public static class Extensions
+{
+    public static IHostBuilder ConfigureAzureKeyVault(this IHostBuilder builder)
+    {
+        return builder.ConfigureAppConfiguration((context, configurationBuilder) =>
+        {
+            if (context.HostingEnvironment.IsProduction())
+            {
+                var configuration = configurationBuilder.Build();
+
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+                _ = configurationBuilder.AddAzureKeyVault(
+                        new Uri($"https://{serviceSettings!.KeyVaultName}.vault.azure.net/"),
+                        new DefaultAzureCredential()
+                    );
+            }
+        });
+    }
+}
